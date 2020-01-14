eea061e9a997b21ec2d68cf43433d2623e7c8124
gnardinosaur <github email address>
2020-01-14 13:45:51 -0500

    fetching and building art objects moved to Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,39 +4,122 @@ import { Dropdown, Checkbox, Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
 import { departmentOptions, dateOptions } from '../constants/searchOptions'
 import { parseSearchDates } from '../helperFunctions';
+import { getRandomInclusive } from '../helperFunctions';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
 
-function Search(props){
+class Search extends React.Component {
 
-  function handleClick(){
-    if (props.dates === "") {
-      props.setSearchDates(-8000, new Date().getFullYear())
-    } else {
-      const parsedDatesArr = parseSearchDates(props.dates)
-      props.setSearchDates(parsedDatesArr[0], parsedDatesArr[1])
-    }
-    props.changeURL("/art")
+  state = {
+    objectIDs: [],
+    threeWorkObjs: []
   }
 
-  return (
-    <div className="search">
-      <h3>Filter Search:</h3>
-      <div className="filters">
-        <Dropdown onChange={props.handleChange} placeholder="Department" name="SET_DEPARTMENT" clearable selection options={departmentOptions} />
-        <Dropdown onChange={props.handleChange} placeholder="Date / Era" name="SET_DATES" clearable selection options={dateOptions} />
-        <div className="reco">
-          <Checkbox onChange={props.handleCheck} name="SET_HIGHLIGHT" toggle />
-          <h4 id="toggle-lbl">Collection Highlight (Recommended)</h4>
+  handleClick = () => {
+    //how can I use thunk or async/await in this function so that store gets udpated and returned BEFORE I call this.fetchArtObjects?
+    if (this.props.dates !== "") {
+      const parsedDatesArr = parseSearchDates(this.props.dates);
+      this.props.setSearchDates(parsedDatesArr[0], parsedDatesArr[1]);
+      this.fetchArtObjects(parsedDatesArr[0], parsedDatesArr[1])
+    } else {
+      this.fetchArtObjects(-8000, new Date().getFullYear())
+    }
+  }
+
+  fetchArtObjects = (start, end) => {
+    let URL;
+    if (this.props.departmentId !== typeof(Number) && this.props.dates === "" && this.props.isHighlight === false) {
+      URL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=*"
+    } else {
+      URL = `https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${this.props.departmentId}&dateBegin=${start}&dateEnd=${end}&isHighlight=${this.props.isHighlight}&q=*`
+    }
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data => this.getThreeIDs(data.objectIDs))
+  }
+
+  getThreeIDs = (objectIDsArr) => {
+    let objectIDs = [];
+    let indexNums = [];    
+    for (let i = 0; i < 3; i++) {
+      indexNums.push(getRandomInclusive(0, objectIDsArr.length))
+    }
+    indexNums.forEach(el => objectIDs.push(objectIDsArr[el]))
+    this.setState({ objectIDs });
+    this.buildWorkObjects()
+  }
+
+  buildWorkObjects = () => {
+    let threeWorkObjs = [];
+    this.state.objectIDs.forEach(el => {
+      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${el}`)
+      .then(resp => resp.json())
+      .then(art => { 
+        let oneWork = {
+          ID: art.objectID,
+          img: art.primaryImage,
+          department: art.department,
+          title: art.title,
+          culture: art.culture,
+          artist: art.artistDisplayName,
+          date: art.objectDate
+        };
+        threeWorkObjs.push(oneWork);
+        this.setState({ threeWorkObjs }, () => {
+          if (this.state.threeWorkObjs.length === 3) {
+            this.addDescriptions();
+          }
+        })
+      })
+    })
+  }
+
+  addDescriptions = () => {
+    let artObjectsWithDescriptions = [];
+    this.state.threeWorkObjs.forEach(el => {
+      axios.get(`https://www.metmuseum.org/art/collection/search/${el.ID}`)
+      .then((resp) => {
+        let html = resp.data;
+        let $ = cheerio.load(html);
+        let textNodes;
+        let itemDescriptionArr = [];
+        $('.artwork__intro__desc').each(function(){ 
+          textNodes = $(this).find('p').contents(); 
+          for (let i = 0; i < textNodes.length; i++) {
+            if (textNodes[i].type === "text") {
+              itemDescriptionArr.push(textNodes[i].data)
+            }
+            el.description = itemDescriptionArr;
+          }
+          artObjectsWithDescriptions.push(el);
+        })
+        this.props.saveArtObjects(artObjectsWithDescriptions)
+      }, this.props.changeURL("/art"))
+    })
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <h3>Filter Search:</h3>
+        <div className="filters">
+          <Dropdown onChange={this.props.handleChange} placeholder="Department" name="SET_DEPARTMENT" clearable selection options={departmentOptions} />
+          <Dropdown onChange={this.props.handleChange} placeholder="Date / Era" name="SET_DATES" clearable selection options={dateOptions} />
+          <div className="reco">
+            <Checkbox onChange={this.props.handleCheck} name="SET_HIGHLIGHT" toggle />
+            <h4 id="toggle-lbl">Collection Highlight (Recommended)</h4>
+          </div>
         </div>
+        <Button onClick={this.handleClick} id="art-btn">Show Me Some Art!</Button>
       </div>
-      <Button onClick={handleClick} id="art-btn">Show Me Some Art!</Button>
-    </div>
-  )
+    )
+  }
 }
 
 function msp(state){
-  const { dates } = state.searchReducer;
-  return { dates }
+  const { departmentId, dates, isHighlight, dateBegin, dateEnd, threeArtObjects } = state.searchReducer;
+  return { departmentId, dates, isHighlight, dateBegin, dateEnd, threeArtObjects }
 }
 
 function mdp(dispatch){
@@ -57,6 +140,12 @@ function mdp(dispatch){
       dispatch({
         type: "SET_SEARCH_DATES",
         payload: { dateBegin, dateEnd }
+      })
+    },
+    saveArtObjects: (objectsArr) => {
+      dispatch({
+        type: "SAVE_ART_OBJECTS",
+        payload: objectsArr 
       })
     }
   }
--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,89 +1,89 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Grid, Image } from 'semantic-ui-react'
-import { getRandomInclusive } from '../helperFunctions';
-import axios from 'axios';
-import cheerio from 'cheerio';
+// import { getRandomInclusive } from '../helperFunctions';
+// import axios from 'axios';
+// import cheerio from 'cheerio';
 
 
 class SearchResult extends React.Component {
 
-  state = {
-    objectIDs: [],
-    threeWorkObjs: []
-  }
+  // state = {
+  //   objectIDs: [],
+  //   threeWorkObjs: []
+  // }
 
-  componentDidMount(){
-    let URL;
-    if (this.props.departmentId !== typeof(Number) && this.props.dates === "" && this.props.isHighlight === false) {
-      URL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=*"
-    } else {
-      URL = `https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${this.props.departmentId}&dateBegin=${this.props.dateBegin}&dateEnd=${this.props.dateEnd}&isHighlight=${this.props.isHighlight}&q=*`
-    }
-    fetch(URL)
-    .then(resp => resp.json())
-    .then(data => this.getThreeIDs(data.objectIDs))
-  }
+  // componentDidMount(){
+  //   let URL;
+  //   if (this.props.departmentId !== typeof(Number) && this.props.dates === "" && this.props.isHighlight === false) {
+  //     URL = "https://collectionapi.metmuseum.org/public/collection/v1/search?q=*"
+  //   } else {
+  //     URL = `https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=${this.props.departmentId}&dateBegin=${this.props.dateBegin}&dateEnd=${this.props.dateEnd}&isHighlight=${this.props.isHighlight}&q=*`
+  //   }
+  //   fetch(URL)
+  //   .then(resp => resp.json())
+  //   .then(data => this.getThreeIDs(data.objectIDs))
+  // }
 
-  getThreeIDs = (objectIDsArr) => {
-    let objectIDs = [];
-    let indexNums = [];    
-    for (let i = 0; i < 3; i++) {
-      indexNums.push(getRandomInclusive(0, objectIDsArr.length))
-    }
-    indexNums.forEach(el => objectIDs.push(objectIDsArr[el]))
-    this.setState({ objectIDs });
-    this.buildWorkObjects()
-  }
+  // getThreeIDs = (objectIDsArr) => {
+  //   let objectIDs = [];
+  //   let indexNums = [];    
+  //   for (let i = 0; i < 3; i++) {
+  //     indexNums.push(getRandomInclusive(0, objectIDsArr.length))
+  //   }
+  //   indexNums.forEach(el => objectIDs.push(objectIDsArr[el]))
+  //   this.setState({ objectIDs });
+  //   this.buildWorkObjects()
+  // }
 
-  buildWorkObjects = () => {
-    let threeWorkObjs = [];
-    this.state.objectIDs.forEach(el => {
-      fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${el}`)
-      .then(resp => resp.json())
-      .then(art => { 
-        let oneWork = {
-          ID: art.objectID,
-          img: art.primaryImage,
-          department: art.department,
-          title: art.title,
-          culture: art.culture,
-          artist: art.artistDisplayName,
-          date: art.objectDate
-        };
-        threeWorkObjs.push(oneWork);
-        this.setState({ threeWorkObjs }, () => {
-          if (this.state.threeWorkObjs.length === 3) {
-            this.addDescriptions();
-          }
-        })
-      })
-    })
-  }
+  // buildWorkObjects = () => {
+  //   let threeWorkObjs = [];
+  //   this.state.objectIDs.forEach(el => {
+  //     fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${el}`)
+  //     .then(resp => resp.json())
+  //     .then(art => { 
+  //       let oneWork = {
+  //         ID: art.objectID,
+  //         img: art.primaryImage,
+  //         department: art.department,
+  //         title: art.title,
+  //         culture: art.culture,
+  //         artist: art.artistDisplayName,
+  //         date: art.objectDate
+  //       };
+  //       threeWorkObjs.push(oneWork);
+  //       this.setState({ threeWorkObjs }, () => {
+  //         if (this.state.threeWorkObjs.length === 3) {
+  //           this.addDescriptions();
+  //         }
+  //       })
+  //     })
+  //   })
+  // }
 
-  addDescriptions = () => {
-    let artObjectsWithDescriptions = [];
-    this.state.threeWorkObjs.forEach(el => {
-      axios.get(`https://www.metmuseum.org/art/collection/search/${el.ID}`)
-      .then((resp) => {
-        let html = resp.data;
-        let $ = cheerio.load(html);
-        let textNodes;
-        let itemDescriptionArr = [];
-        $('.artwork__intro__desc').each(function(){ 
-          textNodes = $(this).find('p').contents(); 
-          for (let i = 0; i < textNodes.length; i++) {
-            if (textNodes[i].type === "text") {
-              itemDescriptionArr.push(textNodes[i].data)
-            }
-            el.description = itemDescriptionArr;
-          }
-          artObjectsWithDescriptions.push(el);
-        })
-        this.setState({ threeWorkObjs: artObjectsWithDescriptions })
-      })
-    })
-  }
+  // addDescriptions = () => {
+  //   let artObjectsWithDescriptions = [];
+  //   this.state.threeWorkObjs.forEach(el => {
+  //     axios.get(`https://www.metmuseum.org/art/collection/search/${el.ID}`)
+  //     .then((resp) => {
+  //       let html = resp.data;
+  //       let $ = cheerio.load(html);
+  //       let textNodes;
+  //       let itemDescriptionArr = [];
+  //       $('.artwork__intro__desc').each(function(){ 
+  //         textNodes = $(this).find('p').contents(); 
+  //         for (let i = 0; i < textNodes.length; i++) {
+  //           if (textNodes[i].type === "text") {
+  //             itemDescriptionArr.push(textNodes[i].data)
+  //           }
+  //           el.description = itemDescriptionArr;
+  //         }
+  //         artObjectsWithDescriptions.push(el);
+  //       })
+  //       this.setState({ threeWorkObjs: artObjectsWithDescriptions })
+  //     })
+  //   })
+  // }
 
   render(){
     return (
@@ -96,7 +96,7 @@ class SearchResult extends React.Component {
             </Grid.Column>
             <Grid.Column width={6}>
               {/* pass in arwork deets & description */}
-              Blah Blah Blah
+              blah blah blah
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -107,8 +107,8 @@ class SearchResult extends React.Component {
 };
 
 function msp(state){
-  const { departmentId, dates, isHighlight, dateBegin, dateEnd } = state.searchReducer;
-  return { departmentId, dates, isHighlight, dateBegin, dateEnd }
+  const { threeArtObjects } = state.searchReducer;
+  return { threeArtObjects }
 };
 
 function mdp(dispatch){
